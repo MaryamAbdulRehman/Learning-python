fe77991949bc07e7f9bd83940c9c3a1847a8f995
Maryam Abdul Rehman <maryamabdulrehman303@gmail.com>
2026-07-24 01:37:36 +0500

    Update objects.py

---
+++ OOP-Learning/objects.py
@@ -0,0 +1,14 @@
+'''
+A snigle class
+'''
+
+class Bags:
+    name="Girly bag"
+    def details():
+        print("This is a girlies bag")
+# ya ha object ban gaya
+leather=Bags()
+# pehly hum asy acess karty thy
+print(Bags.name)
+# lakin jab humny object bana liya to hum object k name sa acess karain Things like that
+print(leather.details)
